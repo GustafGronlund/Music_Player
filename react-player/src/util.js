b2dbceba3cd97fe08242ae1d9eaee03aefc1cfc5
GustafGronlund <gustafgronlund@gmail.com>
2022-04-05 00:03:26 +0200

    mate a object with data for the music

---
+++ react-player/src/util.js
@@ -0,0 +1,71 @@
+import { v4 as uuidv4 } from "uuid";
+
+function chillHop() {
+  return [
+    {
+      name: "avant g",
+      cover: "https://i1.sndcdn.com/artworks-000197662837-kw2g74-t500x500.jpg",
+      artist: "thedaysbefore",
+      audio: ".sounds/avantg.wav",
+      color: ["#fff", "#fff"],
+      id: uuidv4(),
+      active: true,
+    },
+    {
+      name: "it's going to get better",
+      cover: "https://i1.sndcdn.com/artworks-000236229618-xgug16-t500x500.jpg",
+      artist: "thedaysbefore",
+      audio: ".sounds/itsgoingtogetbetter.wav",
+      color: ["#fff", "#fff"],
+      id: uuidv4(),
+      active: true,
+    },
+    {
+      name: "dreams",
+      cover: "https://i1.sndcdn.com/artworks-000190605672-oha2zo-t500x500.jpg",
+      artist: "thedaysbefore",
+      audio: ".sounds/dreams.wav",
+      color: ["#fff", "#fff"],
+      id: uuidv4(),
+      active: true,
+    },
+    {
+      name: "i've played too much fortnite this summer",
+      cover: "https://i1.sndcdn.com/artworks-000400146438-f5ur2r-t500x500.jpg",
+      artist: "thedaysbefore",
+      audio: ".sounds/iveplayedtoomuchfortnitethissummer.wav",
+      color: ["#fff", "#fff"],
+      id: uuidv4(),
+      active: true,
+    },
+    {
+      name: "sunday morning",
+      cover: "https://i1.sndcdn.com/artworks-000224345576-38r2jx-t500x500.jpg",
+      artist: "thedaysbefore",
+      audio: ".sounds/sundaymorning.wav",
+      color: ["#fff", "#fff"],
+      id: uuidv4(),
+      active: true,
+    },
+    {
+      name: "will they return?",
+      cover: "https://i1.sndcdn.com/artworks-000413988732-5tdr95-t500x500.jpg",
+      artist: "thedaysbefore",
+      audio: ".sounds/willtheyreturn.wav",
+      color: ["#fff", "#fff"],
+      id: uuidv4(),
+      active: true,
+    },
+    {
+      name: "the summer is gone",
+      cover: "https://i1.sndcdn.com/artworks-000239806304-n74a8r-t500x500.jpg",
+      artist: "thedaysbefore",
+      audio: ".sounds/thesummerisgone.wav",
+      color: ["#fff", "#fff"],
+      id: uuidv4(),
+      active: true,
+    },
+  ];
+}
+
+export default chillHop;
